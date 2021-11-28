d2a05872dd9ff0de60d33881973d26d1d5144a56
Eitan Gueron <eitangueron@gmail.com>
2021-11-28 16:57:32 +0200

    almost there not yet

--- app/src/main/java/com/example/ex3/MainActivity.java
+++ app/src/main/java/com/example/ex3/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.ex3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startBtn;
+    private Button stopBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startBtn = findViewById(R.id.startBtn);
+        stopBtn = findViewById(R.id.stopBtn);
+
+        // crating notification channel
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("quoteNotification", "quotes channel", NotificationManager.IMPORTANCE_HIGH);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService( new Intent( getBaseContext(), QuotesService.class) );
+            }
+        });
+
+        stopBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService( new Intent( getBaseContext(), QuotesService.class) );
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/ex3/QuotesService.java
@@ -0,0 +1,53 @@
+package com.example.ex3;
+
+import android.app.AlarmManager;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+public class QuotesService extends Service {
+    public QuotesService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        int loopTimeInSecs = 3;      // every 3 secends
+        long loopTimeInMill = 1000 * loopTimeInSecs;
+
+        Toast.makeText( getApplicationContext(), "Started Quotes Service", Toast.LENGTH_SHORT).show();
+
+        AlarmNotificationReciver alarmNotificationReciver = new AlarmNotificationReciver();
+        Intent myIntent = new Intent(this, AlarmNotificationReciver.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, myIntent, 0);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP,
+                SystemClock.elapsedRealtime()+loopTimeInMill,
+                loopTimeInMill,
+                pendingIntent
+        );
+
+        return START_STICKY;
+    }
+
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText( getApplicationContext(), "Stopped Quotes Service", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+    }
+}
